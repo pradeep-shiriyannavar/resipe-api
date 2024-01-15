e9f4c1bb1b867a449b8b2bc161d605c616d431f7
pradeep-shiriyannavar <pradeepshiriyannavar7259.com>
2024-01-15 17:01:26 +0530

    Configured Docker-compose and wait for db

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+calculator function
+"""
+
+
+def add(x, y):
+    """Add x and y retur rsult."""
+    return x+y
+
+
+def substract(x, y):
+
+    return y-x
---
+++ app/tests.py
@@ -0,0 +1,20 @@
+"""
+saple tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_subract_number(self):
+
+        res = calc.substract(10, 15)
+        self.assertEqual(res, 5)
