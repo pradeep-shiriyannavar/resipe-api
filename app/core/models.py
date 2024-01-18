0fd94fec37d2f0e48c73041ad4d416641765aa28
pradeep-shiriyannavar <pradeepshiriyannavar7259.com>
2024-01-18 17:31:53 +0530

    Updated Djago REST Freamwork

--- app/core/models.py
+++ app/core/models.py
@@ -3,11 +3,12 @@ Database models
 """
 
 from django.db import models
-from django.contrib.auth.models import(
+from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin,
 )
+
 
 class UserManager(BaseUserManager):
     """Manager for user"""
