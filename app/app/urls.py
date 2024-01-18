287e0c20062f5a87bcb6a3c6609f1e576032b02e
pradeep-shiriyannavar <pradeepshiriyannavar7259.com>
2024-01-18 18:58:45 +0530

    Updated Djago REST Freamwork

--- app/app/urls.py
+++ app/app/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path(
         'api/docs/',
-         SpectacularSwaggerView.as_view(url_name='api-schema'),
-         name='api-docs',
+        SpectacularSwaggerView.as_view(url_name='api-schema'),
+        name='api-docs',
     )
 ]
