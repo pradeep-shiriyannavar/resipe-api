0295e52dc282dc794d78f3132619d828a1a9409d
pradeep-shiriyannavar <pradeepshiriyannavar7259.com>
2024-02-02 19:20:52 +0530

    Implemented Recipe Listing API

---
+++ app/recipe/serializers.py
@@ -0,0 +1,15 @@
+"""
+Seriazers for recipe API
+"""
+from rest_framework import serializers
+
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Seriazer for recipe"""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only_fields = ['id']
