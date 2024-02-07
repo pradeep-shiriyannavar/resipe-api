3ed9623d17eb421ed3d6c1515224af7cdc5bca25
pradeep-shiriyannavar <pradeepshiriyannavar7259.com>
2024-02-07 23:43:33 +0530

    Created recipe API

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -81,7 +81,7 @@ class PrivateRecipeApiTests(TestCase):
 
     def test_recipe_list_limited_to_user(self):
         """Test retrieving recipes for user"""
-        other_user = create_user(email='other@example.com',password='test123')
+        other_user = create_user(email='other@example.com', password='test123')
         create_recipe(user=other_user)
         create_recipe(user=self.user)
 
