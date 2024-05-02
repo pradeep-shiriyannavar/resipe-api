ee8f76775a8d0e061285977168026962244530ed
pradeep-shiriyannavar <pradeepshiriyannavar7259.com>
2024-05-02 15:35:23 +0530

    added basic tag features

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -9,8 +9,18 @@ from core.models import (
 )
 
 
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tags"""
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields= ['id']
+
+
 class RecipeSerializer(serializers.ModelSerializer):
     """Seriazer for recipe"""
+    tags = TagSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
@@ -19,6 +29,19 @@ class RecipeSerializer(serializers.ModelSerializer):
             ]
         read_only_fields = ['id']
 
+    def create(self, validated_data):
+        """Create a recipe."""
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        auth_user = self.context['request'].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(
+                user=auth_user,
+                **tag,
+            )
+            recipe.tags.add(tag_obj)
+
+        return recipe
 
 class RecipeDetailSerializer(RecipeSerializer):
     """Serialzer for recipe detail view"""
@@ -27,11 +50,4 @@ class RecipeDetailSerializer(RecipeSerializer):
         fields = RecipeSerializer.Meta.fields + ['description']
 
 
-class TagSerializer(serializers.ModelSerializer):
-    """Serializer for tags"""
-
-    class Meta:
-        model = Tag
-        fields = ['id', 'name']
-        read_only_fields= ['id']
 
